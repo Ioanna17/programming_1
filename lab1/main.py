cc9d8ead365cdde0178e64b90e133d57edcc342c
Ivanka <ivasilc23@gmail.com>
2023-09-11 14:03:17 +0300

    add readme file

--- lab1/main.py
+++ lab1/main.py
@@ -1,6 +1,7 @@
 from task_1 import task1
 from task_2 import task2
 from task_3 import task3
+print("Лабораторна робота №1, Варіант 6\nСтудентки Васильчук Іванни, КМ-33\nТема: програмування лінійних алгоритмів та  розгалужених процесів")
 while True:
     choice = input("Введіть номер завдання(1-3): ")
     if choice == "1":
--- lab1/task_3.py
+++ lab1/task_3.py
@@ -3,6 +3,7 @@ from validation import validateInput
 
 def task3():
 
+    print("\n   Якщо число x <=7 буде виконана рівність: 3*x-9 \n   Якщо x >7 буде виконана рівність:1/((x^2)-4")
     x = validateInput(float, "Введіть значення x: ", "Дані не підходять")
     if x <= 7:
         number = 3*x-9
